94b8e571559fa3c01f48496fee20f7b33e7b55a2
burhan <alacaburhan122016@gmail.com>
2023-12-25 02:19:43 +0300

    register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,24 +11,25 @@ const Register = () => {
           <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
             Sign Up
           </h2>
-          <div>
-            <input name="floating_text" type="text" required />
-            <label htmlFor="floating_text">First Name</label>
+          <div className="relative z-0 w-full mb-6 group">
+            <input className="peer" name="floating_text" type="text" required placeholder="" />
+            <label className=""  htmlFor="floating_text">Firts Name</label>
           </div>
-          <div>
-            <input name="floating_text" type="text" required />
-            <label htmlFor="floating_text">Last Name</label>
+          <div className="relative z-0 w-full mb-6 group">
+            <input className="peer" name="floating_text" type="text" required placeholder="" />
+            <label  htmlFor="floating_text">Last Name</label>
           </div>
-          <div>
-            <input name="floating_email" type="email" />
-            <label htmlFor="floating_email">Email</label>
+          <div className="relative z-0 w-full mb-6 group">
+            <input className="peer" name="floating_email" type="email" required placeholder="" />
+            <label  htmlFor="floating_email">Email</label>
           </div>
-          <div>
-            <input name="floating_password" type="password" />
-            <label htmlFor="floating_password">Password</label>
+          <div className="relative z-0 w-full mb-6 group">
+            <input className="peer" name="floating_password" type="password" required placeholder=""/>
+            <label  htmlFor="floating_password">Password</label>
           </div>
-          <button type="submit">Register</button>
-          <button type="button">
+          
+          <button type="submit" className="btn-danger">Register</button>
+          <button type="button" className="btn-danger flex justify-between items-center">
             Continue with Google
             <GoogleIcon color="currentColor" />
           </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
     //! eğer tailwind'in default theme ayarları yerine kendimizinkini eklemek istemiyorsak mutlaka extend objesi içinde eklememiz gerekli yoksa tüm theme objesi değişir ve kendi eklediklerimiz dışındakileri kullanamayız.
     extend: {
       colors: {
-        "gray-dark-main": "#23242a",
+        
+         "gray-dark-main": "#23242a",
         "gray-dark-second": "#28292d",
         "gray-light": "#d3dce6",
         "red-main": "#ff4b45",
@@ -16,3 +17,4 @@ module.exports = {
   plugins: [],
   darkMode: "class",
 };
+
