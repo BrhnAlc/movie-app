bdd291a6bf87faeb3d70db71e62c7a2d9df06b23
burhan <alacaburhan122016@gmail.com>
2023-12-27 02:31:58 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
 import Register from './pages/Register';
 
 function App() {
   return (
     <div className="bg-fuchsia-300 h-screen text-center">
+      <Navbar/>
       <Register/>
     </div>
   );
