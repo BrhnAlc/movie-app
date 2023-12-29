04c8db4bbd34bc748a6b997e21a64c0561593241
burhan <alacaburhan122016@gmail.com>
2023-12-29 04:00:46 +0300

    context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import AuthContextProvider from './context/AuthContext';
 import AppRouter from './router/AppRouter';
 
 function App() {
   return (
     <div className="dark:bg-gray-dark-main min-h-screen">
+      <AuthContextProvider>
       <AppRouter/>
+      </AuthContextProvider>
     </div>
   );
 }
