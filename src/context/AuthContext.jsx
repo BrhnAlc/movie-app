b57016760bbdc98ffc5e1b24d66b72875f149ee6
burhan <alacaburhan122016@gmail.com>
2024-01-01 04:30:45 +0300

    login

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext } from 'react'
 import { auth } from "../auth/firebase";
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword ,signInWithEmailAndPassword} from 'firebase/auth';
 
 
 
@@ -16,12 +16,19 @@ const AuthContextProvider= ({children}) => {
          let userCredential =   await createUserWithEmailAndPassword(auth, email, password); 
          console.log(userCredential);
         } catch (error) {
-           console.log(error); 
+           console.log(error.message); 
         }
-      
-  
     }
-  const values={createUser}
+   const signIn =async(email,password)=>{
+    try {
+        await  signInWithEmailAndPassword(auth, email, password)
+    } catch (error) {
+      console.log(error.message);
+    }
+   }
+
+
+  const values={createUser ,signIn}
   // Bu nesne, AuthContext'in değerine atanacak ve context içinde kullanılabilir.
   return (
     <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
