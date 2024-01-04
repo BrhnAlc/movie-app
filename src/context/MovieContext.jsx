5c3e7fa65245243c41d0f19222e120ab4059a475
burhan <alacaburhan122016@gmail.com>
2024-01-05 02:40:40 +0300

    axios

--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -1,10 +1,24 @@
 
-
-import React, { createContext } from 'react';
-
+import axios from 'axios';
+import React, { createContext, useState } from 'react';
+ 
  export const MovieContext = createContext();
 
+ const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+
+
 const MovieContextProvider = ({children}) => {
+ 
+    const [movie, setMovie] = useState([])
+    
+    const getMovie = (API)=>{
+        axios
+        .get(API)
+        .then((res)=>console.log(res))
+        .catch((err)=>console.log(err));
+    }
+
   return (
     <MovieContext.Provider value={null}>{children}</MovieContext.Provider>
   )
