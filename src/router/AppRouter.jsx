a42b4c52fd445c19b2b6bdfad15dd346b40ae071
burhan <alacaburhan122016@gmail.com>
2024-01-09 00:30:30 +0300

    privaterouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-import {  Routes, Route } from "react-router-dom";
-import Main from "../pages/Main";
 import Navbar from "../components/Navbar";
-import Login from './../pages/Login';
+import Main from "../pages/Main";
+import Login from "../pages/Login";
 import Register from "../pages/Register";
-import MovieDetail from './../pages/MovieDetail';
+import MovieDetail from "../pages/MovieDetail";
+import { Route, Routes } from "react-router-dom";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
-  <>
-  <Navbar/>
-    <Routes>
-           <Route path='/main' element={<Main />} />
-          <Route path='/login' element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="details/:id" element={<MovieDetail />} />
-          
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route element={<PrivateRouter />}>
+          <Route path="/details/:id" element={<MovieDetail />} />
+        </Route>
       </Routes>
-      </>
-    );
+    </>
+  );
 };
 
 export default AppRouter;
